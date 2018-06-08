c60499a5ffab252e5a685a757d282b4da6bfaa07
lietou1986 <lietou1986@qq.com>
2018-06-08 13:43:22 +0800

    update package

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var log4js = require('log4js');
-log4js.configure('./config/log4js.json')
+log4js.configure('./config/log4js.json');
 
 var logger = log4js.getLogger('log');
-logger.setLevel('DEBUG')
+logger.level = 'debug';
 
 module.exports = logger;
